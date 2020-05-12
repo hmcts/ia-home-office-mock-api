5b80cc845ec2e6bcc754e76fedaaf9ab95737a9c
divya.arun <divya.arun@enginegroup.com>
2020-05-12 20:14:18 +0100

    server stub from OpenAPI specification file

---
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficemockapi/infrastructure/api/ApplicationInstructApiController.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.iahomeofficemockapi.infrastructure.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-05-12T20:08:37.834+01:00[Europe/London]")
+
+@Controller
+@RequestMapping("${openapi.appealsInterface.base-path:/v1}")
+public class ApplicationInstructApiController implements ApplicationInstructApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public ApplicationInstructApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
